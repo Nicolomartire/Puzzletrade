4260bce23b1f4c468993db54a2d2804145f65c13
Mir3355 <112379338+Mir3355@users.noreply.github.com>
2022-10-18 10:20:48 +0200

    pages and scss

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ce7a4b320d67b50"; }
+/******/ 		__webpack_require__.h = function() { return "e057380e569ad23b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
-function Page() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
+function HomePage() {
+  return (
+    <div class="wrapper">
+      <header class="header">Puzzle Header</header>
+
+      <ul class="flex-container">
+        <li class="flex-item">1</li>
+        <li class="flex-item">2</li>
+        <li class="flex-item">3</li>
+        <li class="flex-item">4</li>
+        <li class="flex-item">5</li>
+        <li class="flex-item">6</li>
+      </ul>
+      <footer class="footer">Footer</footer>
+    </div>
+  )
 }
-export default Page;
+export default HomePage
