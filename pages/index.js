0efdb0a7a70f2a810da8cc2e8e2b6ab862be28be
Indyglow <matshagglund75@gmail.com>
2022-10-21 10:12:50 +0200

    Update without comments about components

--- pages/index.js
+++ pages/index.js
@@ -97,21 +97,10 @@ export default function Page() {
 	return (
 		<>
 			<div className="wrapper">
-				{/* <header className="header">Puzzle Header</header> */}
+	
 
-				<ul className="flex-container">
-					<li className="flex-item">1</li>
-					<li className="flex-item">2</li>
-					<li className="flex-item">3</li>
-					<li className="flex-item">4</li>
-					<li className="flex-item">5</li>
-					<li className="flex-item">6</li>
-				</ul>
 
-				<footer className="footer">
-					Footer
-					{/* <FooterNav /> */}
-				</footer>
+	
 			</div>
 			<form action="#" method="POST">
 				<div className="formfield">
